c57615baa83219a27e673958e701cd19cfa82d5c
ThisaraSan <you@example.comthisarasandaruwan@gmail.com>
2023-08-07 22:07:27 +0530

    Orders save with transaction

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -5,6 +5,7 @@
 package pos.mvc;
 import pos.mvc.view.CustomerView;
 import pos.mvc.view.ItemView;
+import pos.mvc.view.OrderView;
 
 /**
  *
@@ -17,7 +18,8 @@ public class PosMvc {
      */
     public static void main(String[] args) {
         //new CustomerView().setVisible(true);
-       new ItemView().setVisible(true); 
+       //new ItemView().setVisible(true); 
+       new OrderView().setVisible(true);
     }
     
 }
